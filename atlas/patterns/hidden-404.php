554d3c719e406125c7bfef79bb504ea590cad26a
MaggieCabrera <maggie.cabrera@automattic.com>
2024-04-15 11:20:21 +0200

    testing change

--- atlas/patterns/hidden-404.php
+++ atlas/patterns/hidden-404.php
@@ -4,7 +4,7 @@
  * Slug: atlas/hidden-404
  * Inserter: no
  */
-//test
+//test 
 ?>
 
 <!-- wp:group {"tagName":"main","style":{"dimensions":{"minHeight":"80vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
