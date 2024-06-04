6849429ecbaa512532f5d48f89e4ad71706b339e
MaggieCabrera <maggie.cabrera@automattic.com>
2024-06-04 19:06:13 +0200

    fix border radius

--- term/patterns/outlined-media-and-text.php
+++ term/patterns/outlined-media-and-text.php
@@ -5,26 +5,20 @@
  * Categories: media, text
  */
 ?>
-<!-- wp:columns {"style":{"border":{"width":"2px","radius":"8px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0px"}}},"backgroundColor":"black"} -->
-<div class="wp-block-columns has-black-background-color has-background" style="border-width:2px;border-radius:8px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"blockGap":"0"},"color":{"background":"#edece8"},"border":{"width":"2px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-column has-background" style="border-width:2px;background-color:#edece8;padding-top:0;padding-bottom:0"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<!-- wp:columns {"metadata":{"categories":["media","text"],"patternName":"term/outlined-media-text","name":"Outlined Media and Text"},"align":"wide","style":{"border":{"width":"2px","radius":"8px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0px"}}},"backgroundColor":"black"} -->
+<div class="wp-block-columns alignwide has-black-background-color has-background" style="border-width:2px;border-radius:8px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"style":{"spacing":{"padding":{"bottom":"2px","top":"2px","right":"2px","left":"2px"},"blockGap":"0"}},"backgroundColor":"custom-black","layout":{"type":"default"}} -->
+<div class="wp-block-column has-custom-black-background-color has-background" style="padding-top:2px;padding-right:2px;padding-bottom:2px;padding-left:2px"><!-- wp:group {"style":{"border":{"radius":"4px"}},"backgroundColor":"custom-beige","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-custom-beige-background-color has-background" style="border-radius:4px"><!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
 <p style="margin-top:var(--wp--preset--spacing--60);margin-right:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60);margin-left:var(--wp--preset--spacing--60);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">In his real life, Joel Gavins is actually a tax accountant. True story. You might think it’s the most boring job in the world, but he’ll never go out of business! Taxes are so terribly confusing that everyone needs someone else to do their taxes, that’s the whole point of the industry. So while your job might get taken over by AI, lobbying will always ensure that Joel has a nice cushy job and a working car.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:spacer {"height":"0px"} -->
-<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer --></div>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"border":{"width":"2px"}}} -->
-<div class="wp-block-column" style="border-width:2px">
-
-<!-- wp:cover {"url":"http://wp-dev.test/wp-content/themes/community/term/assets/images/Joel_Gavins.jpg","dimRatio":0,"minHeight":100,"minHeightUnit":"%","isDark":false,"style":{"border":{"radius":"4px"}}} -->
-<div class="wp-block-cover is-light" style="border-radius:4px;min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="http://wp-dev.test/wp-content/themes/community/term/assets/images/Joel_Gavins.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<div class="wp-block-column" style="border-width:2px"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Joel_Gavins.jpg","dimRatio":0,"minHeight":100,"minHeightUnit":"%","isDark":false,"style":{"border":{"radius":"4px"}}} -->
+<div class="wp-block-cover is-light" style="border-radius:4px;min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Joel_Gavins.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
 <p class="has-text-align-center has-large-font-size"></p>
 <!-- /wp:paragraph --></div></div>
-<!-- /wp:cover -->
-
-</div>
+<!-- /wp:cover --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
